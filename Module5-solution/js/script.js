9de4c19086fe3b4407eddc45607c46d7996250f3
LeoPD15 <leon_xlr8@hotmail.com>
2021-02-15 11:10:31 -0600

    module 5 solution

--- Module5-solution/js/script.js
+++ Module5-solution/js/script.js
@@ -102,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-      var chosenCategoryShortName = chooseRandomCategory(categories);
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
       
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -117,10 +117,9 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var CatShortNameMod = "'" + chosenCategoryShortName.short_name + "'";
       
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 
-                                            "randomCategoryShortName", CatShortNameMod);
+                                            "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
